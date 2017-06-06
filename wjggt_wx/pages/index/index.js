b862088ecec0891cab58703806679f27e5821503
李靖 <lijing@camera360.com>
2017-06-07 01:56:31 +0800

    [李靖] 可以保存消费了

--- wjggt_wx/pages/index/index.js
+++ wjggt_wx/pages/index/index.js
@@ -23,13 +23,23 @@ Page({
     app.getUserInfo(function(userInfo){
       //更新数据
       var restaurant = wx.getStorageInfoSync('restaurant');
+
+      //得到用户Id
       that.setData({
         userInfo: userInfo,
         recent: [
           { wday: '星期一', restaurant: '腊肉', 'amount': 120, left: -103, all: '田龙，赵宝旺，王池，冯浩然，敬亮，李靖' },
           { wday: '星期二', restaurant: '绵阳米粉', 'amount': 76, left: -179, all: '田龙，赵宝旺，王池，冯浩然，龚彦铭，李靖' }
         ]
-      })
+      });
+
+      //获取当前用户的消费记录
+      var q0 = new AV.Query('WJConsumeDetail');
+      var usr = AV.Object.createWithoutData('WJUser', userInfo.id);
+      q0.set('user', usr);
+      q0.include('consume');
+      q0.limit(20);
+      q0.find().then();
     });
 
   }
